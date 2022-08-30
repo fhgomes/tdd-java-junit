4c673575d09458426b21a14e964045da83616b90
Fernando Gomes <fernando@playmatch.gg>
2022-08-30 12:38:04 -0300

    refatorando codigo de teste

--- src/test/java/edu/fhg/tdd/CalculadoraTestadaTest.java
+++ src/test/java/edu/fhg/tdd/CalculadoraTestadaTest.java
@@ -3,27 +3,32 @@ package edu.fhg.tdd;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class CalculadoraTestadaTest {
 
+    private CalculadoraTestada target;
+
+    @BeforeEach
+    void setUp() {
+        target = new CalculadoraTestada();
+    }
+
     @Test
     void deveSomar2Mais2Igual4() {
-        CalculadoraTestada target = new CalculadoraTestada();
 
         Assertions.assertEquals(4, target.somar(2, 2));
     }
 
     @Test
     void deveSomar3Mais3Igual6() {
-        CalculadoraTestada target = new CalculadoraTestada();
 
         Assertions.assertEquals(6, target.somar(3, 3));
     }
 
     @Test
     void deveSomarValorFracionado() {
-        CalculadoraTestada target = new CalculadoraTestada();
 
         Assertions.assertEquals(5.5, target.somar(2.5, 3));
     }
